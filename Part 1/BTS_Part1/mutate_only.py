f8666f73652a0726b456b26c421801cd10d43415
IKRAMJAAFAR <u2100632@siswa.um.edu.my>
2024-11-01 10:52:42 +0800

    Changes in BTS PART 1

--- Part 1/BTS_Part1/mutate_only.py
+++ Part 1/BTS_Part1/mutate_only.py
@@ -1,37 +1,23 @@
 import random
 import string
-import datetime
 
 # Target string
 target = "HelloWorld!!"
 target_length = len(target)
 characters = string.ascii_letters + string.punctuation # Possible character set
 
-population_size = 100  # Number of individuals in each generation
-mutation_rate = 0.01  # Probability of mutation for each character
-max_generations = 1000  # Max number of generations to evolve
+def _main_():
+    return 0
+
+def calculate_fitness(individual, target):
+    matches = sum(1 for i in range(len(target)) if individual[i] == target[i])
+    return matches / len(target)  # Normalized fitness between 0 and 1
 
 def generate_individual(length):
     return ''.join(random.choice(characters) for _ in range(length))
 
 def generate_population(size, length):
     return [generate_individual(length) for _ in range(size)]
-
-def calculate_fitness(individual, target):
-    matches = sum(1 for i in range(len(target)) if individual[i] == target[i])
-    return matches / len(target)  # Normalized fitness between 0 and 1
-
-def select_parents(population, fitness_scores):
-    # Select parents using a weighted random choice based on fitness scores
-    weights = [fitness for fitness in fitness_scores]
-    parent1 = random.choices(population, weights=weights, k=1)[0]
-    parent2 = random.choices(population, weights=weights, k=1)[0]
-    return parent1, parent2
-
-def crossover(parent1, parent2): #One-point crossover
-    crossover_point = random.randint(1, len(parent1) - 1)
-    child = parent1[:crossover_point] + parent2[crossover_point:]
-    return child
 
 def mutate(individual, mutation_rate):
     mutated = ''.join(
@@ -40,7 +26,7 @@ def mutate(individual, mutation_rate):
     )
     return mutated
 
-def genetic_algorithm(target, max_generations, population_size, mutation_rate):
+def genetic_algorithm(target, mutation_rate):
     population = generate_population(population_size, len(target))
     generation = 1
     while(True):
@@ -66,9 +52,3 @@ def genetic_algorithm(target, max_generations, population_size, mutation_rate):
         generation += 1
 
     return best_individual
-
-
-then = datetime.datetime.now()
-best_solution = genetic_algorithm(target, max_generations, population_size, mutation_rate)
-now = datetime.datetime.now()
-print("\nBest solution:", best_solution)
